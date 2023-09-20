d49f444045816021c15ded8a62d750cf61da4210
Francisco Soto <fr.sotol@duocuc.cl>
2023-09-20 15:32:39 -0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola</h1>
+    <h2>chupame las bolas</h2>
+    <h3>jaja que chistoso</h3>
+</body>
+</html>

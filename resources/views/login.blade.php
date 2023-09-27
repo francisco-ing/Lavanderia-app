c427b3865f07e1a2158ab776d18e1902ab979092
Francisco Soto <fr.sotol@duocuc.cl>
2023-09-27 02:29:35 -0300

    login y inicio

---
+++ resources/views/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login Page with Background Image Example</title>
+  <link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
+
+</head>
+<body>
+	<div class="form-box">
+        <div id="icono">
+            <img src="https://cdn-icons-png.flaticon.com/512/2230/2230771.png"> 
+        </div>
+		<div class="header-text">
+			<h4>Meli Clean</h4>
+		</div>
+        <input placeholder="Nombre de Usuario" type="text">
+        <input placeholder="Contrasena" type="password">
+        <form action="inicio">
+            <input type="submit" value="Ingresar" />
+        </form>
+	</div>
+</body>
+</html>
